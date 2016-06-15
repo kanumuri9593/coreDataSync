ab57bc8ba2f937d087021484547a46dbe848d5f1
kanumuri9593 <kanumuriyeswanth@gmail.com>
2016-06-14 21:18:26 -0400

    Initial Commit

---
+++ SyncCoredataAlamofire/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SyncCoredataAlamofire
+//
+//  Created by Yeswanth varma Kanumuri on 6/14/16.
+//  Copyright © 2016 Yeswanth varma Kanumuri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
