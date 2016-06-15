d34d690fe979ed14d86fe73eee08d69d08b53a58
kanumuri9593 <yeshu@Yeswanths-MBP.home>
2016-06-15 19:09:09 -0400

    included pod files

---
+++ Pods/Target Support Files/Pods-SyncCoredataAlamofire/Pods-SyncCoredataAlamofire-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_SyncCoredataAlamofireVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_SyncCoredataAlamofireVersionString[];
+
--- SyncCoredataAlamofire/ViewController.swift
+++ SyncCoredataAlamofire/ViewController.swift
@@ -7,11 +7,24 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        Alamofire.request(.GET, "https://httpbin.org/get", parameters: ["foo": "bar"])
+            .responseJSON { response in
+                print(response.request)  // original URL request
+                print(response.response) // URL response
+                print(response.data)     // server data
+                print(response.result)   // result of response serialization
+                
+                if let JSON = response.result.value {
+                    print("JSON: \(JSON)")
+                }
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
